91fb0eeb7bfe3ddf23a399b70e352e7ea99e2814
koowyy <hamza.boussif9654@gmail.com>
2019-05-29 10:18:00 +0000

    mcommitm

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+try {
+    $user='root';
+    $pass='';
+    $cnx = new PDO("mysql:host=localhost;dbname=portfolio", $user, $pass);
+} catch (PDOException $e) {
+    echo "ERROR ".$e->getMessage();
+}
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>My portfolio</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body id="page-top"><a class="menu-toggle rounded" href="#"><i class="fa fa-bars"></i></a>
+    <nav class="navbar navbar-light navbar-expand" id="sidebar-wrapper">
+        <div class="container"><button data-toggle="collapse" class="navbar-toggler d-none" data-target="#"></button>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav sidebar-nav" id="sidebar-nav">
+                    <li class="nav-item sidebar-brand" role="presentation"><a class="nav-link active js-scroll-trigger" href="#page-top">Brand</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#page-top">Home</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#services">Skills</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="d-flex masthead" style="background-image:url('assets/img/bg-masthead.jpg');">
+        <div class="container my-auto text-center">
+            <h1 class="mb-1">Hamza Boussif</h1>
+            <h3 class="mb-5"><em>Portfolio&nbsp;</em></h3>
+        </div>
+    </header>
+    <section id="about" class="content-section bg-light">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-lg-10 mx-auto">
+                    <h1>About Me</h1>
+                    <p class="lead mb-5"><span>Hi there, i'm Hamza Boussif i'm a Youcode student, and a future developer !  </span></p>
+                    <p class="lead mb-5"> <span>
+                        I'm also a Front-end Developer with 1 year of experience designing, developing, and managing simple  sites. Specializes in AngularJS and responsive design.</span></p>
+                     <a class="btn btn-dark btn-xl js-scroll-trigger" role="button" href="#services">RESUME</a>
+                    </div>
+            </div>
+        </div>
+    </section>
+    <section id="services" class="content-section bg-primary text-white text-center" style="background-color: #ecb807 !important;">
+        <div class="container">
+            <div class="content-section-heading">
+                <h3 class="" style="color : #000000 ">show me the code</h3>
+                <h2 class="mb-5" style="color: #000000;">Skills</h2>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0"><span class="mx-auto service-icon rounded-circle mb-3"><i class="fab fa-html5"></i></span>
+                    <h4><strong style="color: #2c3e50;">Html</strong></h4>
+                    <p class="mb-0 text-faded">Advanced skills on html.</p>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0"><span class="mx-auto service-icon rounded-circle mb-3"><i class="fa fa-css3"></i></span>
+                    <h4><strong style="color: #2c3e50;">Css</strong></h4>
+                    <p class="mb-0 text-faded">Freshly redesigned for Bootstrap.</p>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0"><span class="mx-auto service-icon rounded-circle mb-3"><i class="far fa-closed-captioning"></i></span>
+                    <h4><strong style="color: #2c3e50;">C programming</strong></h4>
+                    <p class="mb-0 text-faded"><span>I'm almost in average level of C programming.</span></p>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0"><span class="mx-auto service-icon rounded-circle mb-3"><i class="fab fa-js-square"></i></span>
+                    <h4 style="color: #2c3e50;"><strong style="color: #2c3e50;">Javascript</strong></h4>
+                    <p class="mb-0 text-faded">Animations..Functions..Beautiful functionalities that we can make with JavaScript !</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="portfolio" class="content-section">
+        <div class="container">
+            <div class="content-section-heading text-center">
+                <h3 class="text-secondary mb-0">Portfolio</h3>
+                <h2 class="mb-5">Recent Projects</h2>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="animation/index.html" target="_blank">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h2>Randonnée animation</h2>
+                                <p class="mb-0">We made an awesome website made only with css and html, a homepage with an amazing animation.<br></p>
+                            </div>
+                        </div><img class="img-fluid" src="assets/img/animation.png"></a>
+                </div>
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="donat/index.html" target="_blank">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h2>Vegan Donut Store</h2>
+                                <p class="mb-0">We made the vegan donut store from scratch within a macket of this website.<br></p>
+                            </div>
+                        </div><img class="img-fluid" src="assets/img/donat.png"></a>
+                </div>
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="slack/index.html" target = "_blank">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h2>Slack Home Page</h2>
+                                <p class="mb-0">We recreated the homepage of the slack's website.<br></p>
+                            </div>
+                        </div><img class="img-fluid" src="assets/img/slack.png"></a>
+                </div>
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="count/index.html" target="_blank">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h2>JS Counter</h2>
+                                <p class="mb-0">We made a nice counter with javascript.<br></p>
+                            </div>
+                        </div><img class="img-fluid" src="assets/img/count.png"></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="contact" class="content-section bg-primary text-white" style="width: 100%;background-color: #ecb807 !important;">
+        <div class="container text-center">
+            <h1 class="mb-4" style="color: #000000;">CONTACT ME</h1>
+            <form method="post">
+                <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                <div class="form-group"><input class="form-control" type="text" name="name" placeholder="Name"></div>
+                <div class="form-group"><textarea class="form-control" placeholder="Message" style="height: 130px;" name="message"></textarea></div>
+                <input type="submit" name="submit" class="btn btn-primary btn-block border rounded-0" value="Submit" style="color: #ffffff;background-color: #000000 !important;">
+            </form>
+        </div>
+    </section>
+    <footer class="footer text-center">
+        <div class="container">
+            <ul class="list-inline mb-5">
+                <li class="list-inline-item">&nbsp;<a class="text-white social-link rounded-circle" href="https://www.facebook.com/hamza.9.boussif"><i class="icon-social-facebook"></i></a></li>
+                <li class="list-inline-item">&nbsp;<a class="text-white social-link rounded-circle" href="#"><i class="icon-social-twitter"></i></a></li>
+                <li class="list-inline-item">&nbsp;<a class="text-white social-link rounded-circle" href="https://github.com/ryokushai"><i class="icon-social-github"></i></a></li>
+            </ul>
+            <p class="text-muted mb-0 small">hamza.boussif9654@gmail.com</p>
+            <p class="phone" >06 19 73 76 02</p>
+        </div><a class="js-scroll-trigger scroll-to-top rounded" href="#page-top"><i class="fa fa-angle-up"></i></a></footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
+
+<?php
+
+include'connection.php';
+$pdo=$cnx; 
+if (isset($_POST['submit'])) {
+    $name=$_POST['name'];
+    
+    $email=$_POST['email'];
+  
+    $message=$_POST['message'];
+   
+    
+    $sql='INSERT INTO contact values(NULL,?,?,?)';
+        $q = $pdo->prepare($sql);   
+        $q->execute(array($name,$email,$message));
+
+
+
+            $toEmail = 'hamza.boussif9654@gmail.com';
+                $subject = 'Contact request From '.$name;
+                $body = '<h2>Contact Request</h2>
+                        <h4>Name</h4><p>'.$name.'</p>
+                        <h4>Email</h4><p>'.$email.'</p>
+                        
+                        <h4>Message</h4><p>'.$message.'</p>
+                        ';
+
+                       
+                        $headers = "MIME-Version: 1.0" ."\r\n";
+                        $headers .="Content-Type:text/html;charset=UTF-8" . " \r\n";
+
+                     
+                        $headers .= "from: " .$name. "<".$email.">". "\r\n";
+
+                        if(mail($toEmail, $subject, $body, $headers)){
+                       
+                            $msg = 'email send';
+                            $msgClass = 'alert-success';
+                        } else {
+                            $msg = 'not send';
+                            $msgClass = 'alert-danger';
+
+                        }
+
+
+
+
+    }
+
+?>
